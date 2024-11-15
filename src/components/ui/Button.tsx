285f1b8dbc30a8c6aff3f2f9a7c6d941425bd018
Ye Khaing Win <yekhaingwinn@gmail.com>
2024-11-15 10:45:25 +0800

    add buttonSizeStyles

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,10 +7,9 @@ import { TRounded } from '../../types/rounded.type';
 import classNames from 'classnames';
 import themeConfig from '../../config/theme.config';
 import getColorIntensity from '../../utils/getColorIntensity';
-import getColor from '../../utils/getColor';
 import getColorPreset from '../../utils/getColor';
 
-export type TButtonVariants = 'solid' | 'outline' | 'default';
+export type TButtonVariants = 'solid' | 'outline';
 export type TButtonSize = 'xs' | 'sm' | 'default' | 'lg' | 'xl';
 
 interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
@@ -19,7 +18,8 @@ interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
   className?: string;
   color?: TColor;
   colorIntensity?: TColorIntensity;
-  icon?: TIcon;
+  startIcon?: TIcon;
+  endIcon?: TIcon;
   isActive?: boolean;
   isDisabled?: boolean;
   isLoading?: boolean;
@@ -35,11 +35,11 @@ const Button = forwardRef<HTMLButtonElement, IButtonProps>((props, ref) => {
     className,
     color = 'blue',
     colorIntensity = themeConfig.themeColorShade,
-    icon,
     isActive,
-    isDisable,
+    isDisabled,
     isLoading,
-    rightIcon,
+    startIcon,
+    endIcon,
     rounded = themeConfig.rounded,
     size = 'default',
     variant = 'solid',
@@ -50,13 +50,20 @@ const Button = forwardRef<HTMLButtonElement, IButtonProps>((props, ref) => {
 
   const { textColor } = getColorIntensity(colorIntensity);
 
+  const isIconButton = typeof children === 'undefined';
+
+  /**
+   * Variant
+   */
   const buttonVariant: { [key in TButtonVariants]: string } = {
     solid: classNames(
       // Default
       { [bg.default.pure]: !isActive },
       [borderWidth, border.default.pure],
       [textColor],
+      // Hover
       [bg.hover.shade, border.hover.shade],
+      // Active
       [bg.active.shade, border.active.shade],
       {
         [bg.default.shade]: isActive,
@@ -64,19 +71,124 @@ const Button = forwardRef<HTMLButtonElement, IButtonProps>((props, ref) => {
       },
     ),
     // Default
-    outline: '',
-    default: '',
+    outline: classNames(
+      'bg-transparent',
+      'text-black dark:text-white',
+      borderWidth,
+      {
+        [border.default.transparent]: !isActive,
+      },
+      // Hover
+      [border.hover.pure],
+      // Active
+      [border.active.pure],
+      {
+        [border.default.pure]: isActive,
+      },
+    ),
   };
 
   const buttonVariantStyles = buttonVariant[variant as TButtonVariants];
+
+  /**
+   * Size
+   */
+
+  const buttonSize: {
+    [key in TButtonSize]: {
+      general: string;
+      startIcon: string;
+      endIcon: string;
+    };
+  } = {
+    xs: {
+      general: classNames(
+        {
+          'px-3': !isIconButton,
+          'px-0.5': isIconButton,
+        },
+        'py-0.5 text-xs',
+      ),
+      startIcon: classNames({ 'mr-1': !isIconButton }, 'text-[1.125rem]'),
+      endIcon: classNames('ml-1', 'text-[1.125rem]'),
+    },
+    sm: {
+      general: classNames(
+        {
+          'px-4': !isIconButton,
+          'px-1': isIconButton,
+        },
+        'py-1',
+        'text-sm',
+      ),
+      startIcon: classNames({ 'mr-1': !isIconButton }, 'text-[1.25rem]'),
+      endIcon: classNames('ml-1', 'text-[1.25rem]'),
+    },
+    default: {
+      general: classNames(
+        {
+          'px-5': !isIconButton,
+          'px-1.5': isIconButton,
+        },
+        'py-1.5',
+        'text-base',
+      ),
+      startIcon: classNames({ 'mr-1.5': !isIconButton }, 'text-[1.5rem]'),
+      endIcon: classNames('ml-1.5', 'text-[1.5rem]'),
+    },
+    lg: {
+      general: classNames(
+        {
+          'px-6': !isIconButton,
+          'px-2': isIconButton,
+        },
+        'py-2',
+        'text-lg',
+      ),
+      startIcon: classNames({ 'mr-2': !isIconButton }, 'text-[1.75rem]'),
+      endIcon: classNames('ml-2', 'text-[1.75rem]'),
+    },
+    xl: {
+      general: classNames(
+        {
+          'px-7': !isIconButton,
+          'px-2.5': isIconButton,
+        },
+        'py-2.5',
+        'text-xl',
+      ),
+      startIcon: classNames({ 'mr-2.5': !isIconButton }, 'text-[1.75rem]'),
+      endIcon: classNames('ml-2.5', 'text-[1.75rem]'),
+    },
+  };
+
+  const buttonSizeStyles = buttonSize[size].general;
+  const buttonStartIconStyles = buttonSize[size].startIcon;
+  const buttonEndIconStyles = !isIconButton
+    ? buttonSize[size].endIcon
+    : undefined;
+
+  const buttonDisabledStyles = classNames('opacity-50', 'pointer-events-none');
+
+  const styles = classNames(
+    'inline-flex items-center justify-center',
+    buttonVariantStyles,
+    buttonSizeStyles,
+    rounded,
+    themeConfig.transition,
+    { [buttonDisabledStyles]: isDisabled || isLoading },
+    className,
+  );
 
   return (
     <button
       ref={ref}
       data-component-name="Button"
-      className={buttonVariantStyles}
+      type="button"
+      className={styles}
       {...rest}
     >
+      {(!!startIcon || isLoading) && <}
       {children}
     </button>
   );
